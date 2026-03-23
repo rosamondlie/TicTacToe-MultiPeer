e3e9a8192a616a8a37a540eaffd0ea1a2d72c9a7
rosamondlie <rosamond.patricia.01@gmail.com>
2026-03-23 22:56:07 +0700

    vid 3 - two player game mode

--- Game Screen/GameView.swift
+++ Game Screen/GameView.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct GameView: View {
+    @EnvironmentObject var game: GameService
     @Environment(\.dismiss) var dismiss
     var body: some View {
-        VStack{
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if [game.player1.isCurrent, game.player2.isCurrent].allSatisfy({ $0 == false}){
+                Text("Select a player to start")
+            }
+            HStack {
+                Button(game.player1.name){
+                    game.player1.isCurrent = true
+                }
+                .buttonStyle(PlayerButtonStyle(isCurrent: game.player1.isCurrent))
+                Button(game.player2.name){
+                    game.player2.isCurrent = true
+                }
+                .buttonStyle(PlayerButtonStyle(isCurrent: game.player2.isCurrent))
+            }
+            .disabled(game.gameStarted)
+            VStack {
+                HStack {
+                    ForEach(0...2, id: \.self){
+                        index in SquareView(index: index)
+                    }
+                }
+                HStack {
+                    ForEach(3...5, id: \.self){
+                        index in SquareView(index: index)
+                    }
+                }
+                HStack {
+                    ForEach(6...8, id: \.self){
+                        index in SquareView(index: index)
+                    }
+                }
+            }
+            .disabled(game.boardDisabled)
+            VStack {
+                if game.gameOver {
+                    Text("Game Over")
+                    if game.possibleMoves.isEmpty {
+                        Text("Nobody wins")
+                    } else {
+                        Text("\(game.currentPlayer.name) wins!")
+                    }
+                    Button("New Game") {
+                        game.reset()
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+            }
+            .font(.largeTitle)
+            Spacer()
         }
+    
         .toolbar{
             ToolbarItem(placement: .navigationBarTrailing){
                 Button("End Game"){
@@ -22,10 +71,25 @@ struct GameView: View {
             }
         }
         .navigationTitle("Xs and Os")
+        .onAppear {
+            game.reset()
+        }
         .inNavigationStack()
     }
 }
 
 #Preview {
     GameView()
+        .environmentObject(GameService())
+}
+
+struct PlayerButtonStyle: ButtonStyle {
+    let isCurrent: Bool
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding(8)
+            .background(RoundedRectangle(cornerRadius: 10)
+                .fill(isCurrent ? Color.green : Color.gray))
+            .foregroundColor(.white)
+    }
 }
---
+++ Game Screen/SquareView.swift
@@ -0,0 +1,30 @@
+//
+//  SquareView.swift
+//  XsandOs
+//
+//  Created by Rosamond Patricia Selamat Lie on 23/03/26.
+//
+
+import SwiftUI
+
+struct SquareView: View {
+    @EnvironmentObject var game: GameService
+    let index: Int
+    var body: some View {
+        Button {
+            game.makeMove(at: index)
+        } label: {
+            game.gameBoard[index].image
+                .resizable()
+                .frame(width: 100, height: 100)
+        }
+        .disabled(game.gameBoard[index].player != nil)
+        .foregroundColor(.primary)
+        
+    }
+}
+
+#Preview {
+    SquareView(index: 1)
+        .environmentObject(GameService())
+}
